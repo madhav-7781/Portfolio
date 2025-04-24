5b57a241f7709fbf87b6e9d26fd84f34a57c8593
madhav-7781 <madhavverma079@gmail.com>
2025-04-24 20:43:58 +0530

    commit

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,11 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import path from "path";
+
 
 const app = express();
+app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
